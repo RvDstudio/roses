a258e75e9bc027653193e5ea461fd16ddf204693
Reinier <39220977+RvDstudio@users.noreply.github.com>
2022-01-10 20:43:40 +0100

    Update

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,3 @@
-import { Logo } from "@/components/index";
 import { useShoppingCart } from "@/hooks/use-shopping-cart";
 import { formatCurrency } from "@/lib/utils";
 import { ShoppingCartIcon } from "@heroicons/react/solid";
@@ -10,15 +9,19 @@ const Header = () => {
   return (
     <header className="sticky top-0 bg-white z-10 shadow">
       <div className="container xl:max-w-screen-xl mx-auto p-6 flex justify-between">
-        <Logo />
+        <Link href="/">
+          <a className="text-3xl">
+            Qmatic<span className="text-green-600">|Store</span>
+          </a>
+        </Link>
         <Link href="/cart">
-          <a className="flex items-center space-x-1 text-gray-700 hover:text-gray-900">
+          <a className="flex items-center space-x-1 text-green-600 hover:text-green-700">
             <div className="relative">
               <ShoppingCartIcon className="w-7 h-7 flex-shrink-0" />
             </div>
             <p className="text-lg">
               {formatCurrency(totalPrice)}{" "}
-              <span className="text-sm text-gray-500">({cartCount})</span>
+              <span className="text-sm text-green-600">({cartCount})</span>
             </p>
           </a>
         </Link>
