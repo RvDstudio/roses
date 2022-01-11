2c57b41ca5e60f8ac05c48e7a8aa3ba721d65bba
Reinier <39220977+RvDstudio@users.noreply.github.com>
2022-01-12 00:13:56 +0100

    Summary

--- components/Header.js
+++ components/Header.js
@@ -1,32 +1,80 @@
 import { useShoppingCart } from "@/hooks/use-shopping-cart";
-import { formatCurrency } from "@/lib/utils";
+import { SearchIcon } from "@heroicons/react/outline";
 import { ShoppingCartIcon } from "@heroicons/react/solid";
+import MenuIcon from "@mui/icons-material/Menu";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { IconButton } from "@mui/material";
 import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-  const { totalPrice, cartCount } = useShoppingCart();
-
+  const { cartCount } = useShoppingCart();
   return (
-    <header className="sticky top-0 bg-white z-10 shadow">
-      <div className="container xl:max-w-screen-xl mx-auto p-6 flex justify-between">
-        <Link href="/">
-          <a className="text-3xl">
-            Qmatic<span className="text-green-600">|Store</span>
-          </a>
-        </Link>
-        <Link href="/cart">
-          <a className="flex items-center space-x-1 text-green-600 hover:text-green-700">
-            <div className="relative">
-              <ShoppingCartIcon className="w-7 h-7 flex-shrink-0" />
-            </div>
-            <p className="text-lg">
-              {formatCurrency(totalPrice)}{" "}
-              <span className="text-sm text-green-600">({cartCount})</span>
-            </p>
-          </a>
-        </Link>
-      </div>
-    </header>
+    <>
+      <header className="sticky top-0 z-50 mb-6 border-b-2 border-gray-100 bg-white">
+        <div className="flex items-center justify-between  px-4  flex-grow py-4">
+          <div className="mt-2 flex items-center mr-4 text-[#002c4c]">
+            <Link href="/">
+              <p className="text-2xl md:text-3xl items-center font-semibold ml-2">
+                <span className="">Qmatic</span>
+                <span className="text-green-600 font-normal py-2">|Store</span>
+              </p>
+            </Link>
+          </div>
+
+          <div className="hidden md:flex flex-grow items-center border-1 rounded-md bg-gray-100 py-2 md:shadow-sm">
+            <input
+              className="pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400 flex-grow"
+              type="text"
+              placeholder="Start your search"
+            />
+            <SearchIcon className="hidden md:inline-flex h-8 text-white bg-green-600 rounded-lg p-2 cursor-pointer md:mx-2" />
+          </div>
+
+          {/* Bottom Nav */}
+          <div className="hidden md:flex items-center space-x-3 p-2 pl-6  text-gray-500 text-md">
+            <p className="link cursor-pointer">Home</p>
+            <p className="link cursor-pointer">Projects</p>
+            <p className="link cursor-pointer">Gallery</p>
+            <p className="link cursor-pointer hidden lg:inline-flex">Blog</p>
+            <p className="link cursor-pointer hidden lg:inline-flex">About</p>
+            <p className="link cursor-pointer hidden lg:inline-flex">Contact</p>
+          </div>
+          <div className="relative z-1">
+            <IconButton aria-label="User" className="hover:text-green-600">
+              <PermIdentityIcon
+                aria-label="User"
+                className="h-5 w-5 text-gray-700 hover:text-green-600"
+              />
+            </IconButton>
+            <IconButton aria-label="Settings" className="hover:text-green-600">
+              <SettingsIcon className="h-5 w-5 text-gray-700 hover:text-green-600" />
+            </IconButton>
+            <IconButton
+              aria-label="Nav"
+              className="md:hidden hover:text-green-600"
+            >
+              <MenuIcon className="h-5 w-5 text-gray-700 hover:text-green-600" />
+            </IconButton>
+            <IconButton>
+              <Link href="/cart">
+                <a className=" space-x-1 text-green-600 hover:text-green-600">
+                  <div className="relative">
+                    <p className=" text-lg">
+                      <span className="z-20 absolute bottom-0 left-4 text-sm text-red-600 rounded-full p-2">
+                        ({cartCount})
+                      </span>
+                    </p>
+                    <ShoppingCartIcon className="w-7 h-7 flex-shrink-0" />
+                  </div>
+                </a>
+              </Link>
+            </IconButton>
+          </div>
+        </div>
+      </header>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import { useState } from 'react';
-import { ProductCard } from '@/components/index';
-
-import products from 'products';
+import { ProductCard } from "@/components/index";
+import products from "products";
+import { useState } from "react";
 
 export default function Home() {
   const [disabled, setDisabled] = useState(false);
@@ -9,7 +8,7 @@ export default function Home() {
   return (
     <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {products.map(product => (
+        {products.map((product) => (
           <ProductCard
             key={product.id}
             disabled={disabled}
